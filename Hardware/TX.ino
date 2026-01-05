5275ff7f60fac96a457a324fcf6e38499844e44d
Bryan Spannaus <bryanspannaus@gmail.com>
2026-01-05 10:20:41 -0500

    Update TX.ino

--- Hardware/TX.ino
+++ Hardware/TX.ino
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <cstdint>
 #include <vector>
+#include <SD.h>
+#include <TC74.h>
 
 
 //Radio Pins
@@ -18,50 +20,42 @@
 
 //Output Pins
 #define BUZZER 0
+#define SDcs 22
 
 //Lat and Long transmit points
 #define TRANSMISSION_LONG
 #define TRANSMISSION_LAT
 
-//Data Structures
-struct HEADER {
-  float SYSBAT;
-  int UID;
-};
 
-class DATA_BUFFER {
-  public:
+struct DATA_BUFFER {
   float SEND_RPM; //RPMs
-  float BATT_TEMP; 
-  float MOTOR_TEMP;
-  float AMBIENT_TEMP;
-  float AMP_LOAD;
-  float VOLTAGE;
-  float LAT; //GPS data
+  float MOTOR_TEMP; //Motor
+  float AMBIENT_TEMP; //From MCU
+  float AMP_LOAD; 
+  float VOLTAGE; //Batt voltage
+  float LAT;
   float LONG;
-  float SPEED;
-  float X_ROT; //Gyroscope data
-  float Y_ROT;
-  float Z_ROT;
   float X_ACEL; //Accelerometer data
   float Y_ACEL;
-  float Z_ACEL;
   int UTC_HOUR; //UTC time from GPS
   int UTC_MINUTE;
   int UTC_SECOND;
-  int LAP; //# of laps calculated by # of buffers cleared +1
+  int LAP; //# of laps calculated by GPS
   int LAP_TIME;
   int TOTAL_TIME; 
+  int LoRa_SNR; //SNR (Signal to Noise Ratio)
 };
 
-
-void addPoint(std::vector<DATA_BUFFER>*, int, struct DATA_BUFFER*);
+void transmit(float param1, float param2, float param3, float param4);
+void saveData(struct DATA_BUFFER *foo);
 
 //RPM Calculating Variables (used in hallISR, and Loop)
 volatile unsigned long lastPulseTime = 0;
 volatile unsigned long pulseInterval = 0;
 volatile bool newPulse = false;
 
+
+TC74 dvc(0x48); //Initalizes TC74A03.3VAT as a sensor
 
 RH_RF95 rf95(RF95_CS, RF95_INT);
 
@@ -81,28 +75,23 @@ void setup() {
   pinMode(RPM_SENSE, INPUT_PULLUP);
   pinMode(RF95_RST, OUTPUT);
   digitalWrite(RF95_RST, HIGH);
-
+  SPI.begin(); 
+  SD.begin(SDcs);
   attachInterrupt(digitalPinToInterrupt(RPM_SENSE), hallISR, FALLING);
-
   Serial.begin(115200);
-
-  Serial.println("LoRa TX using default SPI");
-
+  dvc.begin(); //Start Temparature Sensor
   // Reset LoRa module
   digitalWrite(RF95_RST, LOW);
   delay(10);
   digitalWrite(RF95_RST, HIGH);
   delay(10);
 
-  SPI.begin(); 
-
   if (!rf95.init()) { //init fail
     Serial.println("LoRa radio init failed");
     while (1);
   }
   
-  Serial.println("LoRa radio init OK!");
-  
+  Serial.println("LoRa radio init OK!");  
   if (!rf95.setFrequency(RF95_FREQ)) { 
     Serial.println("setFrequency failed");
     while (1);
@@ -112,6 +101,14 @@ void setup() {
   Serial.println(RF95_FREQ);
 
   rf95.setTxPower(20, false);
+  if (SD.exists("/telem")) {
+  } else {
+  SD.mkdir("telemDATA");
+  }
+
+  File dataLog = SD.open("TELEM.csv", FILE_WRITE);
+  dataLog.println("RPM, M_TMP, A_TMP, AMPS, VOLTS, LAT, LONG, X_ACEL, Y_ACEL, UTC_HR, UTC_MIN, UTC_SEC, LAP, LAPT_TIME, TIME, LORA_SNR"); //Generate Header for file
+  dataLog.close(); //Write changes to file
 
   digitalWrite(BUZZER, HIGH);
   delay(150);
@@ -119,18 +116,14 @@ void setup() {
 }
 
 void loop() {
+  float AMPS;
   int lastUpdateTime;
   float battVoltage;
+  float temp;
 
-  bool dataSet; //which struct 0 or 1
+  struct DATA_BUFFER TEMP_BUF;
 
-  std::vector<struct DATA_BUFFER> buf1[440];
-  std::vector<struct DATA_BUFFER> buf0[440];
-  struct HEADER header;
-  struct DATA_BUFFER temp;
-
-
-  unsigned long lastCalcTime = 0;
+  //unsigned long lastCalcTime = 0;
   float rpm = 0.0;
 
   if (newPulse) {
@@ -143,65 +136,36 @@ void loop() {
       rpm = (60.0 * 1000000.0) / interval; // 1 pulse per rev
     }
   }
-
-  // Print RPM every 500 ms
-  if (millis() - lastCalcTime >= 500) {
-    lastCalcTime = millis();
-    Serial.print("RPM: ");
-    Serial.println(rpm, 1);
-  }
-
+  TEMP_BUF.SEND_RPM = rpm;
 
   battVoltage = (analogRead(VOLTAGE_DIVIDER) * 24.8)/1005;
+  TEMP_BUF.VOLTAGE = battVoltage;
 
-/*
+  if (millis() >= lastUpdateTime + 1000) {
+    transmit(AMPS, battVoltage, rpm, temp);
+    lastUpdateTime = millis(); //Reset counter
+  }
 
-sprintf(radiopacket, "%f, %i,", rpm, AMPS);
-Serial.println(radiopacket);
-*/
-  //rf95.send((uint8_t *)radiopacket, 127);
+  temp = dvc.readTemperature('c');
+  TEMP_BUF.MOTOR_TEMP = temp;
+
+  saveData(&TEMP_BUF);
+}
+
+void transmit(float param1, float param2, float param3, float param4) {
+  float data[4];
+  char dataSendSTR[25]; //Init 25 byte string with 4 bytes padding (assuming 1 byte per char)
+  data[0] = param1;
+  data[1] = param2;
+  data[2] = param3;
+  data[3] = param4;
+  sprintf(dataSendSTR, "%f.2, %f.2, %f.1, %f.1,\0", data[1], data[2], data[3], data[4]); //Format data
+  rf95.send((uint8_t *)dataSendSTR, sizeof(dataSendSTR)); //Send to LoRa buffers
   rf95.waitPacketSent();
-  
-
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
-
-if (millis() >= lastUpdateTime + 1000) {
-  lastUpdateTime = millis();
-  if(dataSet == false) {
-  //addPoint(*data0);
-  } else {
-  //addPoint(*data1);
- }
 }
 
-}
-
-void transmit() {
-
-  
-}
-
-void addPoint(std::vector<DATA_BUFFER> s, int idx, DATA_BUFFER* foo){
-  s[idx].SEND_RPM = foo->SEND_RPM;
-  s[idx].BATT_TEMP = foo->BATT_TEMP;
-  s[idx].MOTOR_TEMP = foo->MOTOR_TEMP;
-  s[idx].AMBIENT_TEMP = foo->AMBIENT_TEMP;
-  s[idx].AMP_LOAD = foo->AMP_LOAD;
-  s[idx].VOLTAGE = foo->VOLTAGE;
-  s[idx].LAT = foo->LAT;
-  s[idx].LONG = foo->LONG;
-  s[idx].SPEED = foo->SPEED;
-  s[idx].X_ROT = foo->X_ROT;
-  s[idx].Y_ROT = foo->Y_ROT;
-  s[idx].Z_ROT = foo->Z_ROT;
-  s[idx].X_ACEL = foo->X_ACEL;
-  s[idx].Y_ACEL = foo->Y_ACEL;
-  s[idx].Z_ACEL = foo->Z_ACEL;
-  s[idx].UTC_HOUR = foo->UTC_HOUR;
-  s[idx].UTC_MINUTE = foo->UTC_MINUTE;
-  s[idx].UTC_SECOND = foo->UTC_SECOND;
-  s[idx].LAP = foo->LAP;
-  s[idx].LAP_TIME = foo->LAP_TIME;
-  s[idx].TOTAL_TIME = foo->TOTAL_TIME;
+void saveData(struct DATA_BUFFER *foo) {
+  File dataLog = SD.open("TELEM.csv", FILE_WRITE);
+  dataLog.printf("%f.1, %f.1, %f.1, %f.2, %f.2, %f.6, %f.6, %f.3, %f.3, %i, %i, %i, %i, %i, %i, %i", foo->SEND_RPM, foo->MOTOR_TEMP, foo->AMBIENT_TEMP, foo->AMP_LOAD, foo->VOLTAGE, foo->LAT, foo->LONG, foo->X_ACEL, foo->Y_ACEL, foo->UTC_HOUR, foo->UTC_MINUTE, foo->UTC_SECOND, foo->LAP, foo->LAP_TIME, foo->TOTAL_TIME, foo->LoRa_SNR);
+  dataLog.close();
 }
