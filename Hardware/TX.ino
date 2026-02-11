ed82e5091e9087a9df1f6c5569b778092027d3d5
Bryan Spannaus <bryanspannaus@gmail.com>
2026-02-11 13:49:24 -0500

    Update TX.ino

--- Hardware/TX.ino
+++ Hardware/TX.ino
@@ -20,7 +20,7 @@
 
 //Output Pins
 #define BUZZER 0
-#define SDcs 22
+#define SDcs 7
 
 //Lat and Long transmit points
 #define TRANSMISSION_LONG
@@ -46,7 +46,7 @@ struct DATA_BUFFER {
   int LoRa_SNR; //SNR (Signal to Noise Ratio)
 };
 
-void transmit(float param1, float param2, float param3, float param4);
+void transmit(float param1, float param2, float param3, float param4, struct DATA_BUFFER *foo);
 void saveData(struct DATA_BUFFER *foo);
 
 //RPM Calculating Variables (used in hallISR, and Loop)
@@ -58,7 +58,7 @@ volatile bool newPulse = false;
 TC74 dvc(0x48); //Initalizes TC74A03.3VAT as a sensor
 
 RH_RF95 rf95(RF95_CS, RF95_INT);
-
+//GPT Code
 void hallISR() {
   unsigned long now = micros();
   if (lastPulseTime != 0) {
@@ -67,9 +67,12 @@ void hallISR() {
   }
   lastPulseTime = now;
 }
+//GPT Code End
 
 void setup() {
-
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(RF95_CS, OUTPUT);
+  pinMode(SDcs, OUTPUT);
   pinMode(BUZZER, OUTPUT);
   pinMode(VOLTAGE_DIVIDER, INPUT);
   pinMode(RPM_SENSE, INPUT_PULLUP);
@@ -81,10 +84,14 @@ void setup() {
   Serial.begin(115200);
   dvc.begin(); //Start Temparature Sensor
   // Reset LoRa module
+  digitalWrite(SDcs, HIGH);
+
+  digitalWrite(RF95_CS, LOW);
   digitalWrite(RF95_RST, LOW);
   delay(10);
   digitalWrite(RF95_RST, HIGH);
   delay(10);
+  digitalWrite(RF95_CS, HIGH);
 
   if (!rf95.init()) { //init fail
     Serial.println("LoRa radio init failed");
@@ -100,22 +107,25 @@ void setup() {
   Serial.print("Set Freq to: ");
   Serial.println(RF95_FREQ);
 
+  digitalWrite(SDcs, LOW);
   rf95.setTxPower(20, false);
   if (SD.exists("/telem")) {
   } else {
-  SD.mkdir("telemDATA");
+  SD.mkdir("telem");
   }
 
   File dataLog = SD.open("TELEM.csv", FILE_WRITE);
   dataLog.println("RPM, M_TMP, A_TMP, AMPS, VOLTS, LAT, LONG, X_ACEL, Y_ACEL, UTC_HR, UTC_MIN, UTC_SEC, LAP, LAPT_TIME, TIME, LORA_SNR"); //Generate Header for file
   dataLog.close(); //Write changes to file
+  digitalWrite(SDcs, HIGH);
 
   digitalWrite(BUZZER, HIGH);
-  delay(150);
+  delay(200);
   digitalWrite(BUZZER, LOW);
 }
 
 void loop() {
+  //Declare Variables
   float AMPS;
   int lastUpdateTime;
   float battVoltage;
@@ -125,7 +135,7 @@ void loop() {
 
   //unsigned long lastCalcTime = 0;
   float rpm = 0.0;
-
+  //GPT Code
   if (newPulse) {
     noInterrupts();
     unsigned long interval = pulseInterval;
@@ -136,36 +146,34 @@ void loop() {
       rpm = (60.0 * 1000000.0) / interval; // 1 pulse per rev
     }
   }
+  //GPT Code End
   TEMP_BUF.SEND_RPM = rpm;
 
-  battVoltage = (analogRead(VOLTAGE_DIVIDER) * 24.8)/1005;
+  battVoltage = (analogRead(VOLTAGE_DIVIDER) * 24.8)/1005; //I have no idea where this proportion came from, testing
   TEMP_BUF.VOLTAGE = battVoltage;
-
-  if (millis() >= lastUpdateTime + 1000) {
-    transmit(AMPS, battVoltage, rpm, temp);
-    lastUpdateTime = millis(); //Reset counter
-  }
 
   temp = dvc.readTemperature('c');
   TEMP_BUF.MOTOR_TEMP = temp;
 
-  saveData(&TEMP_BUF);
+  transmit(AMPS, battVoltage, rpm, temp, &TEMP_BUF); 
 }
 
-void transmit(float param1, float param2, float param3, float param4) {
-  float data[4];
+void transmit(float param1, float param2, float param3, float param4, struct DATA_BUFFER *foo) {
+  digitalWrite(RF95_CS, LOW); //Enable RF95 (Pull SPI CS Pin LOW)
+  Serial.println("Transmitting!"); 
   char dataSendSTR[25]; //Init 25 byte string with 4 bytes padding (assuming 1 byte per char)
-  data[0] = param1;
-  data[1] = param2;
-  data[2] = param3;
-  data[3] = param4;
-  sprintf(dataSendSTR, "%f.2, %f.2, %f.1, %f.1,\0", data[1], data[2], data[3], data[4]); //Format data
+  sprintf(dataSendSTR, "%f.2, %f.2, %f.1, %f.1,\0", param1, param2, param3, param4); //Format data
   rf95.send((uint8_t *)dataSendSTR, sizeof(dataSendSTR)); //Send to LoRa buffers
-  rf95.waitPacketSent();
+  rf95.waitPacketSent(); 
+  digitalWrite(RF95_CS, HIGH); //Disable RF95 (Push SPI CS Pin HIGH)
+  saveData(foo); //Save to SD Card
 }
 
 void saveData(struct DATA_BUFFER *foo) {
+  digitalWrite(SDcs, LOW); //Enable SD Card (Pull SPI CS Pin LOW)
   File dataLog = SD.open("TELEM.csv", FILE_WRITE);
   dataLog.printf("%f.1, %f.1, %f.1, %f.2, %f.2, %f.6, %f.6, %f.3, %f.3, %i, %i, %i, %i, %i, %i, %i", foo->SEND_RPM, foo->MOTOR_TEMP, foo->AMBIENT_TEMP, foo->AMP_LOAD, foo->VOLTAGE, foo->LAT, foo->LONG, foo->X_ACEL, foo->Y_ACEL, foo->UTC_HOUR, foo->UTC_MINUTE, foo->UTC_SECOND, foo->LAP, foo->LAP_TIME, foo->TOTAL_TIME, foo->LoRa_SNR);
   dataLog.close();
+  digitalWrite(SDcs, HIGH); //Disable SD Card (Push SPI CS Pin HIGH)
+  loop();
 }
